816ec9199fced53fa18d10ded5682fb58fb3fa66
munnukumar <50713536+munnukumar@users.noreply.github.com>
2024-04-03 17:35:25 +0530

    task_4

--- food-shop/index.js
+++ food-shop/index.js
@@ -11,3 +11,12 @@ basketHeading.style.color = "green";
 
 const thanks = document.getElementById("thanks");
 thanks.innerHTML = "<p>Please visit us again</p>";
+
+
+
+const fruit = document.getElementsByClassName("fruit");
+fruit[2].style.background = "yellow";
+
+for(let i = 0; i<fruit.length; i++){
+  fruit[i].style.fontWeight = "bold"
+}
