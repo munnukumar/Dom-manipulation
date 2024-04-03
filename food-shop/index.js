882d194db7984b5ddbc2d520c656b9fe2e0cd6cf
munnukumar <50713536+munnukumar@users.noreply.github.com>
2024-04-04 01:46:12 +0530

    task_10

--- food-shop/index.js
+++ food-shop/index.js
@@ -66,3 +66,52 @@ for(let i = 0; i<oddFruit.length; i++){
 }
 
 
+//Add & Delete Elements
+// Add the Edit Button:
+const li = document.getElementsByClassName('fruit');
+for (let i = 0; i < li.length; i++) {
+    const edit = document.createElement("button");
+    const editText = document.createTextNode("Edit");
+    edit.append(editText);
+    edit.className = "edit-btn";
+    li[i].appendChild(edit);
+}
+
+// Implement the code as in video but with one extra 'Edit' button in 'li'
+
+const form = document.querySelector("form");
+const fruits = document.querySelector(".fruits");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const fruitToAdd = document.getElementById("fruit-to-add");
+    const newFruit = document.createElement("li");
+    newFruit.className = "fruit";
+    newFruit.innerHTML = fruitToAdd.value + '<button class="delete-btn">x</button><button class="edit-btn">Edit</button>';
+    fruits.appendChild(newFruit);
+});
+
+  
+  
+    // const newLiText = document.createTextNode('fruitToAdd.value');
+    // newLi.appendChild(newLiText);
+    // newLi.className = 'fruit';
+
+    // const deleteBtn = document.createElement('button')
+    // const deleteBtnText = document.createTextNode('edit');
+    // deleteBtn.appendChild(deleteBtnText);
+    // deleteBtn.className = "delete-btn";
+
+    // newLi.appendChild(deleteBtn);
+    // fruits.appendChild(newLi);
+
+// })
+
+fruits.addEventListener('click', function(event){
+    if(event.target.classList.contains('delete-btn')){
+        const fruitToDelete = event.target.parentElement;
+        fruits.removeChild(fruitToDelete);
+    }
+})
+
+
+
