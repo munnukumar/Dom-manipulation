214f7517ad0cd6160ed9f5324f91e96e3662130c
munnukumar <50713536+munnukumar@users.noreply.github.com>
2024-04-03 17:15:29 +0530

    task_3

---
+++ food-shop/index.js
@@ -0,0 +1,13 @@
+const heading = document.getElementById("header");
+heading.style.backgroundColor = "green";
+heading.style.borderBottom = "3px solid orange";
+
+const mainHeading = document.getElementById("main-heading");
+mainHeading.textContent = "Fruit World";
+mainHeading.style.color = "orange";
+
+const basketHeading = document.getElementById("basket-heading");
+basketHeading.style.color = "green";
+
+const thanks = document.getElementById("thanks");
+thanks.innerHTML = "<p>Please visit us again</p>";
