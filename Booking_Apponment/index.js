790937092351db2ac71994be14de9c6fb556420c
munnukumar <50713536+munnukumar@users.noreply.github.com>
2024-04-04 18:29:15 +0530

    task_16

--- Booking_Apponment/index.js
+++ Booking_Apponment/index.js
@@ -109,3 +109,51 @@ function handleFormSubmit(event) {
 }
 
 module.exports = handleFormSubmit;
+
+
+// task_16 (Having Edit Functionality)
+// In this task you want to provide the user a facility that if has misspelled some information, then he is able to correct it via an edit button.
+
+// Deliverables:
+// 1. Make sure that after form submission all the input fields are empty again.
+// 2. Inside the 'li' add an edit button after the delete button.
+// 3. When you click on this edit button, the user details should be removed from the screen and from the local storage and should populate the input fields with the existing values.
+
+// Write your code below:
+function handleFormSubmit(event) {
+    event.preventDefault();
+    let myObj = {
+        username: event.target.username.value,
+        email: event.target.email.value,
+        phone: event.target.phone.value,
+    }
+    let myObj_serialised = JSON.stringify(myObj);
+    localStorage.setItem(event.target.email.value, myObj_serialised);
+    const ul = document.querySelector("ul");
+    const li = document.createElement("li");
+
+   li.innerHTML = `${event.target.username.value} - ${event.target.email.value} - ${event.target.phone.value} <button type='button' class='delete-button' data-email='${event.target.email.value}'>Delete</button> <button type='button' class='edit-button' data-email='${event.target.email.value}'>Edit</button>`;
+    li.id = event.target.email.value;
+    ul.appendChild(li);
+
+    const deleteButton = li.querySelector(".delete-button");
+
+  deleteButton.addEventListener("click", function (){
+    const email = deleteButton.getAttribute('data-email');
+        document.getElementById(email).remove();
+        localStorage.removeItem(email);
+  });
+
+   const editButton = li.querySelector(".edit-button");
+    editButton.addEventListener("click", function () {
+        const email = editButton.getAttribute('data-email');
+        const getItem = JSON.parse(localStorage.getItem(email));
+        document.getElementById("username").value = getItem.username;
+        document.getElementById("email").value = getItem.email;
+        document.getElementById("phone").value = getItem.phone;
+        document.getElementById(email).remove();
+        localStorage.removeItem(email);
+    });
+}
+
+module.exports = handleFormSubmit;
